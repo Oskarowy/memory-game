ebaf08c88881dbea580f6c059a61b9867e4932da
Oski <oskar.bialek.programista@gmail.com>
2017-09-21 19:24:39 +0200

    added sound of game beggining

--- Unit1.cpp
+++ Unit1.cpp
@@ -2,16 +2,39 @@
 
 #include <vcl.h>
 #pragma hdrstop
+#include "mmsystem.h"
 
 #include "Unit1.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int liczby[1000];
+bool gra_rozpoczeta = false;
+
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
     : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+    randomize();
+    for(int i=0; i<1000; i++)
+    {
+        liczby[i] = random(5)+1;
+    }
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Label1Click(TObject *Sender)
+{
+    if(gra_rozpoczeta==false)
+    {
+        gra_rozpoczeta = true;
+        sndPlaySound("snd/start.wav", SND_ASYNC);
+    }
+}
+//---------------------------------------------------------------------------
--- Unit1.h
+++ Unit1.h
@@ -25,6 +25,8 @@ __published:	// IDE-managed Components
     TImage *z3a;
     TImage *z4a;
     TImage *z5a;
+    void __fastcall FormCreate(TObject *Sender);
+    void __fastcall Label1Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
     __fastcall TForm1(TComponent* Owner);
